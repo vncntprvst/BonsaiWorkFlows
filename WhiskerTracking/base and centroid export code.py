2f81c6f1bc37e8ca3d6ba02d08fa11cbf69d3fad
Vincent Prevosto <vincent.prevosto@duke.edu>
2019-04-17 18:53:13 -0400

    updated whisker B+C export

--- WhiskerTracking/base and centroid export code.py
+++ WhiskerTracking/base and centroid export code.py
@@ -12,26 +12,26 @@ def process(sortedRegions):
   # Order component list by decreasing area size
   #largest = list(Enumerable.OrderByDescending(sortedRegions, lambda x:x.Area))
   # Order component list by centroid position
-  hSortRegionList = list(Enumerable.OrderBy(sortedRegions, lambda x:x.Centroid.X))
-  #vSortRegionList = list(Enumerable.OrderByDescending(sortedRegions, lambda x:x.Centroid.Y))
-  if len(hSortRegionList) >=6:
-    baseList=Tuple.Create(Point2f(hSortRegionList[0].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[1].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[2].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[3].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[4].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[5].Contour.ToArray[Point]()[0]))
-    centroidList=Tuple.Create(Point2f(hSortRegionList[0].Centroid.X,hSortRegionList[0].Centroid.Y),Point2f(hSortRegionList[1].Centroid.X,hSortRegionList[1].Centroid.Y),Point2f(hSortRegionList[2].Centroid.X,hSortRegionList[2].Centroid.Y),Point2f(hSortRegionList[3].Centroid.X,hSortRegionList[3].Centroid.Y),Point2f(hSortRegionList[4].Centroid.X,hSortRegionList[4].Centroid.Y),Point2f(hSortRegionList[5].Centroid.X,hSortRegionList[5].Centroid.Y))
-  if len(hSortRegionList) ==5:
-    baseList=Tuple.Create(Point2f(hSortRegionList[0].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[1].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[2].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[3].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[4].Contour.ToArray[Point]()[0]),NaNPoint)
-    centroidList=Tuple.Create(Point2f(hSortRegionList[0].Centroid.X,hSortRegionList[0].Centroid.Y),Point2f(hSortRegionList[1].Centroid.X,hSortRegionList[1].Centroid.Y),Point2f(hSortRegionList[2].Centroid.X,hSortRegionList[2].Centroid.Y),Point2f(hSortRegionList[3].Centroid.X,hSortRegionList[3].Centroid.Y),Point2f(hSortRegionList[4].Centroid.X,hSortRegionList[4].Centroid.Y),NaNPoint)
-  if len(hSortRegionList) ==4:
-    baseList=Tuple.Create(Point2f(hSortRegionList[0].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[1].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[2].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[3].Contour.ToArray[Point]()[0]),NaNPoint,NaNPoint)
-    centroidList=Tuple.Create(Point2f(hSortRegionList[0].Centroid.X,hSortRegionList[0].Centroid.Y),Point2f(hSortRegionList[1].Centroid.X,hSortRegionList[1].Centroid.Y),Point2f(hSortRegionList[2].Centroid.X,hSortRegionList[2].Centroid.Y),Point2f(hSortRegionList[3].Centroid.X,hSortRegionList[3].Centroid.Y),NaNPoint,NaNPoint)
-  if len(hSortRegionList) ==3:
-    baseList=Tuple.Create(Point2f(hSortRegionList[0].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[1].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[2].Contour.ToArray[Point]()[0]),NaNPoint,NaNPoint,NaNPoint)
-    centroidList=Tuple.Create(Point2f(hSortRegionList[0].Centroid.X,hSortRegionList[0].Centroid.Y),Point2f(hSortRegionList[1].Centroid.X,hSortRegionList[1].Centroid.Y),Point2f(hSortRegionList[2].Centroid.X,hSortRegionList[2].Centroid.Y),NaNPoint,NaNPoint,NaNPoint)
-  elif len(hSortRegionList) ==2:
-    baseList=Tuple.Create(Point2f(hSortRegionList[0].Contour.ToArray[Point]()[0]),Point2f(hSortRegionList[1].Contour.ToArray[Point]()[0]),NaNPoint,NaNPoint,NaNPoint,NaNPoint)
-    centroidList=Tuple.Create(Point2f(hSortRegionList[0].Centroid.X,hSortRegionList[0].Centroid.Y),Point2f(hSortRegionList[1].Centroid.X,hSortRegionList[1].Centroid.Y),NaNPoint,NaNPoint,NaNPoint,NaNPoint)
-  elif len(hSortRegionList) == 1:
-    baseList=Tuple.Create(Point2f(hSortRegionList[0].Contour.ToArray[Point]()[0]),NaNPoint,NaNPoint,NaNPoint,NaNPoint,NaNPoint)
-    centroidList=Tuple.Create(Point2f(hSortRegionList[0].Centroid.X,hSortRegionList[0].Centroid.Y),NaNPoint,NaNPoint,NaNPoint,NaNPoint,NaNPoint) 
+  #hSortRegionList = list(Enumerable.OrderBy(sortedRegions, lambda x:x.Centroid.X))
+  vSortRegionList = list(Enumerable.OrderByDescending(sortedRegions, lambda x:x.Centroid.Y))
+  if len(vSortRegionList) >=6:
+    baseList=Tuple.Create(Point2f(vSortRegionList[0].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[1].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[2].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[3].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[4].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[5].Contour.ToArray[Point]()[0]))
+    centroidList=Tuple.Create(Point2f(vSortRegionList[0].Centroid.X,vSortRegionList[0].Centroid.Y),Point2f(vSortRegionList[1].Centroid.X,vSortRegionList[1].Centroid.Y),Point2f(vSortRegionList[2].Centroid.X,vSortRegionList[2].Centroid.Y),Point2f(vSortRegionList[3].Centroid.X,vSortRegionList[3].Centroid.Y),Point2f(vSortRegionList[4].Centroid.X,vSortRegionList[4].Centroid.Y),Point2f(vSortRegionList[5].Centroid.X,vSortRegionList[5].Centroid.Y))
+  elif len(vSortRegionList) ==5:
+    baseList=Tuple.Create(Point2f(vSortRegionList[0].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[1].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[2].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[3].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[4].Contour.ToArray[Point]()[0]),NaNPoint)
+    centroidList=Tuple.Create(Point2f(vSortRegionList[0].Centroid.X,vSortRegionList[0].Centroid.Y),Point2f(vSortRegionList[1].Centroid.X,vSortRegionList[1].Centroid.Y),Point2f(vSortRegionList[2].Centroid.X,vSortRegionList[2].Centroid.Y),Point2f(vSortRegionList[3].Centroid.X,vSortRegionList[3].Centroid.Y),Point2f(vSortRegionList[4].Centroid.X,vSortRegionList[4].Centroid.Y),NaNPoint)
+  elif len(vSortRegionList) ==4:
+    baseList=Tuple.Create(Point2f(vSortRegionList[0].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[1].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[2].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[3].Contour.ToArray[Point]()[0]),NaNPoint,NaNPoint)
+    centroidList=Tuple.Create(Point2f(vSortRegionList[0].Centroid.X,vSortRegionList[0].Centroid.Y),Point2f(vSortRegionList[1].Centroid.X,vSortRegionList[1].Centroid.Y),Point2f(vSortRegionList[2].Centroid.X,vSortRegionList[2].Centroid.Y),Point2f(vSortRegionList[3].Centroid.X,vSortRegionList[3].Centroid.Y),NaNPoint,NaNPoint)
+  elif len(vSortRegionList) ==3:
+    baseList=Tuple.Create(Point2f(vSortRegionList[0].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[1].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[2].Contour.ToArray[Point]()[0]),NaNPoint,NaNPoint,NaNPoint)
+    centroidList=Tuple.Create(Point2f(vSortRegionList[0].Centroid.X,vSortRegionList[0].Centroid.Y),Point2f(vSortRegionList[1].Centroid.X,vSortRegionList[1].Centroid.Y),Point2f(vSortRegionList[2].Centroid.X,vSortRegionList[2].Centroid.Y),NaNPoint,NaNPoint,NaNPoint)
+  elif len(vSortRegionList) ==2:
+    baseList=Tuple.Create(Point2f(vSortRegionList[0].Contour.ToArray[Point]()[0]),Point2f(vSortRegionList[1].Contour.ToArray[Point]()[0]),NaNPoint,NaNPoint,NaNPoint,NaNPoint)
+    centroidList=Tuple.Create(Point2f(vSortRegionList[0].Centroid.X,vSortRegionList[0].Centroid.Y),Point2f(vSortRegionList[1].Centroid.X,vSortRegionList[1].Centroid.Y),NaNPoint,NaNPoint,NaNPoint,NaNPoint)
+  elif len(vSortRegionList) == 1:
+    baseList=Tuple.Create(Point2f(vSortRegionList[0].Contour.ToArray[Point]()[0]),NaNPoint,NaNPoint,NaNPoint,NaNPoint,NaNPoint)
+    centroidList=Tuple.Create(Point2f(vSortRegionList[0].Centroid.X,vSortRegionList[0].Centroid.Y),NaNPoint,NaNPoint,NaNPoint,NaNPoint,NaNPoint) 
   else:
     baseList=Tuple.Create(NaNPoint,NaNPoint,NaNPoint,NaNPoint,NaNPoint,NaNPoint)
     centroidList=Tuple.Create(NaNPoint,NaNPoint,NaNPoint,NaNPoint,NaNPoint,NaNPoint)
